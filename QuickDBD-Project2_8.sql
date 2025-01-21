7fe06a9abee365e64c358dd6503118d884dedf62
bryguy911 <bjm1187@outlook.com>
2025-01-20 18:16:56 -0600

    Uploading SQL database files

---
+++ QuickDBD-Project2_8.sql
@@ -0,0 +1,51 @@
+﻿-- Exported from QuickDBD: https://www.quickdatabasediagrams.com/
+-- Link to schema: https://app.quickdatabasediagrams.com/#/d/CngbeH
+-- NOTE! If you have used non-SQL datatypes in your design, you will have to change these here.
+
+-- Exported from QuickDBD: https://www.quickdatabasediagrams.com/
+-- Link to schema: https://app.quickdatabasediagrams.com/
+-- NOTE! If you have used non-SQL datatypes in your design, you will have to change these here.
+
+CREATE TABLE "category" (
+    "category_id" CHAR(4)   NOT NULL,
+    "category" VARCHAR(15)   NOT NULL
+);
+
+CREATE TABLE "subcategory" (
+    "subcategory_id" VARCHAR(6)   NOT NULL,
+    "subcategory" VARCHAR(15)   NOT NULL
+);
+
+CREATE TABLE "contacts" (
+    "contact_id" INT   NOT NULL,
+    "first_name" VARCHAR(12)   NOT NULL,
+    "last_name" VARCHAR(12)   NOT NULL,
+    "email" VARCHAR(50)   NOT NULL
+);
+
+CREATE TABLE "campaign" (
+    "cf_id" INT   NOT NULL,
+    "contact_id" INT   NOT NULL,
+    "company_name" VARCHAR(50)   NOT NULL,
+    "description" VARCHAR(50)   NOT NULL,
+    "goal" DECIMAL   NOT NULL,
+    "pledged" DECIMAL   NOT NULL,
+    "outcome" VARCHAR(20)   NOT NULL,
+    "backers_count" VARCHAR(20)   NOT NULL,
+    "country" CHAR(2)   NOT NULL,
+    "currency" CHAR(3)   NOT NULL,
+    "launch_date" DATE   NOT NULL,
+    "end_date" DATE   NOT NULL,
+    "category_id" CHAR(4)   NOT NULL,
+    "subcategory_id" VARCHAR(6)   NOT NULL
+);
+
+ALTER TABLE "campaign" ADD CONSTRAINT "fk_campaign_contact_id" FOREIGN KEY("contact_id")
+REFERENCES "contacts" ("contact_id");
+
+ALTER TABLE "campaign" ADD CONSTRAINT "fk_campaign_category_id" FOREIGN KEY("category_id")
+REFERENCES "category" ("category_id");
+
+ALTER TABLE "campaign" ADD CONSTRAINT "fk_campaign_subcategory_id" FOREIGN KEY("subcategory_id")
+REFERENCES "subcategory" ("subcategory_id");
+
